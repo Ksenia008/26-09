a6d45dc3d74ab819bc1385a4cea846a1f4211f8b
Ksenia008 <114093379+Ksenia008@users.noreply.github.com>
2022-09-26 18:43:29 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+using namespace std;
+int main() {
+	/* int a;
+	cin >> a;
+	if (a > 0) {//true
+	a += 1;
+
+	}
+	cout << a << endl;
+	*/
+	/*int a;
+	cin >> a;
+	if (a > 0) {//true
+		a += 1;
+	}
+	else {
+		a -= 2;//a=a-2;
+	}
+	cout << a << endl;*/
+	/*int a;
+	cin >> a;
+	if (a > 0) { a += 1;
+	}
+	else if (a < 0) { a -= 2;
+	}
+	else if (a == 0) { a = 10;
+	}
+	cout << a << endl;
+	*/
+	//if4
+	/* 
+	int a, b, c;
+	int k = 0;
+	cin >> a >> b >> c;
+	if (a > 0) {
+		k++;
+	}
+	if (b > 0) { k++;
+	}
+	if (c > 0) { k++;
+	}
+	cout << k << endl;
+	*/
+	//if6int a, b, c;
+	/*int a, b ;
+	cin >> a >> b;
+	if (a > b) {
+		cout << a << endl;
+	}
+	else if (b > a) {
+		cout << b << endl;
+
+	}
+	else if (a == b) { cout << "equais" << endl;
+	} */
+	//7
+	/*int a, b;
+	cin>>a>>b;
+	if (a < b) { cout << a << endl;
+	}
+	if (b < a) { cout << b << endl;
+	}
+	if (a == b) { cout << "equals" << endl;
+	} */
+	int a, b;
+	cin >> a >> b;
+	if (a > b) { cout << a << b << endl;
+	}
+	if (b > a) { cout << b << a << endl;
+	}
+}
+
+	
